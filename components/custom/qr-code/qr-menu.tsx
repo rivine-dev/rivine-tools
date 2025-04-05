cd0734922f79b3a13cb76a71cf828bf99c8930f9
harivignesh0101 <harivignesh0101@gmail.com>
2025-04-05 12:01:09 +0530

    added: localization for qr menu

--- components/custom/qr-code/qr-menu.tsx
+++ components/custom/qr-code/qr-menu.tsx
@@ -7,35 +7,18 @@ import * as React from "react";
 import {MenuButton, MenuItem} from "@/components/ui/menu-button";
 import {Card} from "@/components/ui/card";
 import {getNonLocalizedSlug} from "@/i18n/get-localized-path";
-
-const qrUrl = '/qr-code-generator';
-
-const sidebarItems = [
-    {
-        icon: LinkIcon,
-        title: "URL",
-        href: `${qrUrl}`,
-    },
-    {
-        icon: Type,
-        title: "Text",
-        href: `${qrUrl}/text`,
-    },
-    {
-        icon: PhoneIcon,
-        title: "Phone",
-        href: `${qrUrl}/phone`,
-    },
-]
+import {useTranslations} from "use-intl";
+import {qrMenuItems} from "@/config/site-config";
 
 export function QrMenu() {
     const pathname = usePathname()
+    const t = useTranslations()
 
     return (
         <div className="p-2 min-w-[10rem]">
             <Card className="py-0 h-[100%]">
                 <div className="flex overflow-x-auto md:flex-col md:overflow-visible px-2 lg:py-2">
-                    {sidebarItems.map((item) => {
+                    {qrMenuItems.map((item) => {
                         const isActive = getNonLocalizedSlug(pathname) === `/${item.href}` || getNonLocalizedSlug(pathname) === item.href
 
                         return (
@@ -46,7 +29,7 @@ export function QrMenu() {
                                             <item.icon className="size-4"/>
                                         </div>
                                         <div className="flex flex-col gap-0.5 leading-none">
-                                            <span className="font-semibold">{item.title}</span>
+                                            <span className="font-semibold">{t(item.title)}</span>
                                         </div>
                                     </Link>
                                 </MenuButton>
