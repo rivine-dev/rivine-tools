dfb920bbb69ad7fc215fdd30ce89fc521d60f299
harivignesh0101 <harivignesh0101@gmail.com>
2025-04-17 11:23:42 +0530

    fixed: canonical url for home page

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,50 +1,47 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {clsx, type ClassValue} from "clsx"
+import {twMerge} from "tailwind-merge"
 import {supportedLanguages} from "@/i18n/config";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
 }
 
 
 export function formatTime(seconds: number): string {
-  const hours = Math.floor(seconds / 3600)
-  const mins = Math.floor((seconds % 3600) / 60)
-  const secs = seconds % 60
+    const hours = Math.floor(seconds / 3600)
+    const mins = Math.floor((seconds % 3600) / 60)
+    const secs = seconds % 60
 
-  if (hours > 0) {
-    return `${hours}:${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`
-  }
+    if (hours > 0) {
+        return `${hours}:${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`
+    }
 
-  return `${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`
+    return `${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`
 }
 
 
 export function generateAlternates(locale: string, slug: string) {
-  const defaultLang = supportedLanguages.find((lang) => lang.default)?.code || "en";
+    const defaultLang = supportedLanguages.find((lang) => lang.default)?.code || "en";
 
-  // Generate canonical URL
-  let canonical = locale === defaultLang ? `${slug}` : `/${locale}${slug}`;
-  if (!canonical) {
-    canonical = '/';
-  }
-
-  // Generate alternate URLs excluding the current locale and x-default
-  const languages = supportedLanguages.reduce<Record<string, string>>((acc, lang) => {
-    // Add alternate URLs for all languages except the current locale
-    if (lang.code !== locale) {
-      acc[lang.code] =
-          lang.code === defaultLang ? `${slug}` : `/${lang.code}${slug}`;
-      if (!acc[lang.code]) {
-        acc[lang.code] = '/';
-      }
+    let canonical = locale === defaultLang ? `${slug}` : `/${locale}${slug}`;
+    if (!canonical) {
+        canonical = '/';
     }
-    return acc;
-  }, {});
 
-  return {
-    canonical,
-    languages,
-  };
+
+    const languages = supportedLanguages.reduce<Record<string, string>>((acc, lang) => {
+        acc[lang.code] =
+            lang.code === defaultLang ? `${slug}` : `/${lang.code}${slug}`;
+        if (!acc[lang.code]) {
+            acc[lang.code] = '/';
+        }
+        return acc;
+    }, {
+        "x-default": `${slug ? slug : '/'}`,
+    });
+
+    return {
+        canonical,
+        languages,
+    };
 }
-
